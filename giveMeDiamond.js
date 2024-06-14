470a4e6d864c9d8b30b4416279fa87b32c3d9bc9
InshutiSouvede <isouvede2@gmail.com>
2024-06-14 17:05:42 +0200

    Give me a Diamond

---
+++ giveMeDiamond.js
@@ -0,0 +1,12 @@
+function diamond(n) {
+    if (n % 2 === 0 || n <= 0) return null
+    let dia = ''; const tmp = 2 * n
+    for (i = 1; i <= tmp; i += 2) {
+        let str = ''
+        const j = i > n ? tmp - i : i
+        str = str.padStart(j, '*')
+        str = str.padStart(((n - j) / 2) + j, ' ')
+        dia += str + '\n'
+    }
+    return dia;
+}
