6de00cfaf94a62720f92834bf4823e85f1af2ca5
InshutiSouvede <isouvede2@gmail.com>
2024-06-27 15:57:32 +0200

    refactor Jokes you've been 'awaiting' for ... promise

--- awaitJokes.js
+++ awaitJokes.js
@@ -1,26 +1,21 @@
 async function sayJoke(apiUrl, jokeId){
-    try{
-      let data = await fetch(apiUrl)    
-      data = await data.json()
-      if(!data.jokes){
-        throw new Error("No jokes at url: "+apiUrl)
-      }
-      data = data.jokes.filter((el)=>el.id === jokeId)
-      if(data.length==0){
-        throw new Error(`No jokes found id: ${jokeId}`)
-      }
-      return Promise.resolve({
-        saySetup:()=>{      
-        return data[0].setup
-        },
-        sayPunchLine:()=>{      
-        return data[0].punchLine
-        }
-      })
-    }catch(err){
-      //pass rejected msg
-      throw new Error(err.message)
-    }
-   
-    
+  let data = await fetch(apiUrl)    
+  data = await data.json()
+  if(!data.jokes){
+    throw new Error("No jokes at url: "+apiUrl)
   }
+  data = data.jokes.filter((el)=>el.id === jokeId)
+  if(data.length==0){
+    throw new Error(`No jokes found id: ${jokeId}`)
+  }
+  return {
+    saySetup:()=>{      
+    return data[0].setup
+    },
+    sayPunchLine:()=>{      
+    return data[0].punchLine
+    }
+  }
+
+
+}
